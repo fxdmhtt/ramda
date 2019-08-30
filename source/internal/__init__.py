a15d63c0b4e3634e5c831425e6599541f98dadbd
Junkang <yuanjunkang@gmail.com>
2019-08-30 19:07:46 +0800

    -

--- source/internal/__init__.py
+++ source/internal/__init__.py
@@ -4,4 +4,14 @@ class JSObject:
     def __init__(self, d):
         self.__dict__.update(d)
 
-undefined = JSObject({'@@functional/undefined': True})
+    def __getattr__(self, name):
+        return None
+
+class undefined(JSObject):
+    def __init__(self):
+        super().__init__({'@@functional/undefined': True})
+
+    def __repr__(self):
+        return 'undefined'
+
+undefined = undefined()
--- source/internal/_arity.py
+++ source/internal/_arity.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 
 def _arity(n, fn):
+    from .. import __
     if n == 0: return lambda: fn()
-    elif n == 1: return lambda a0: fn(a0)
-    elif n == 2: return lambda a0, a1: fn(a0, a1)
-    elif n == 3: return lambda a0, a1, a2: fn(a0, a1, a2)
-    elif n == 4: return lambda a0, a1, a2, a3: fn(a0, a1, a2, a3)
-    elif n == 5: return lambda a0, a1, a2, a3, a4: fn(a0, a1, a2, a3, a4)
-    elif n == 6: return lambda a0, a1, a2, a3, a4, a5: fn(a0, a1, a2, a3, a4, a5)
-    elif n == 7: return lambda a0, a1, a2, a3, a4, a5, a6: fn(a0, a1, a2, a3, a4, a5, a6)
-    elif n == 8: return lambda a0, a1, a2, a3, a4, a5, a6, a7: fn(a0, a1, a2, a3, a4, a5, a6, a7)
-    elif n == 9: return lambda a0, a1, a2, a3, a4, a5, a6, a7, a8: fn(a0, a1, a2, a3, a4, a5, a6, a7, a8)
-    elif n == 10: return lambda a0, a1, a2, a3, a4, a5, a6, a7, a8, a9: fn(a0, a1, a2, a3, a4, a5, a6, a7, a8, a9)
+    elif n == 1: return lambda a0=__: fn(a0)
+    elif n == 2: return lambda a0=__, a1=__: fn(a0, a1)
+    elif n == 3: return lambda a0=__, a1=__, a2=__: fn(a0, a1, a2)
+    elif n == 4: return lambda a0=__, a1=__, a2=__, a3=__: fn(a0, a1, a2, a3)
+    elif n == 5: return lambda a0=__, a1=__, a2=__, a3=__, a4=__: fn(a0, a1, a2, a3, a4)
+    elif n == 6: return lambda a0=__, a1=__, a2=__, a3=__, a4=__, a5=__: fn(a0, a1, a2, a3, a4, a5)
+    elif n == 7: return lambda a0=__, a1=__, a2=__, a3=__, a4=__, a5=__, a6=__: fn(a0, a1, a2, a3, a4, a5, a6)
+    elif n == 8: return lambda a0=__, a1=__, a2=__, a3=__, a4=__, a5=__, a6=__, a7=__: fn(a0, a1, a2, a3, a4, a5, a6, a7)
+    elif n == 9: return lambda a0=__, a1=__, a2=__, a3=__, a4=__, a5=__, a6=__, a7=__, a8=__: fn(a0, a1, a2, a3, a4, a5, a6, a7, a8)
+    elif n == 10: return lambda a0=__, a1=__, a2=__, a3=__, a4=__, a5=__, a6=__, a7=__, a8=__, a9=__: fn(a0, a1, a2, a3, a4, a5, a6, a7, a8, a9)
     else: raise ValueError('First argument to _arity must be a non-negative integer no greater than ten')
