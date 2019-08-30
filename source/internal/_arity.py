aee0c950c7e4101faa3ce35d813c1431eedbd1d0
Junkang <yuanjunkang@gmail.com>
2019-08-30 16:30:31 +0800

    bugfix

--- source/internal/_arity.py
+++ source/internal/_arity.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 
-def _arity(*arguments):
-    n, fn, *_ = arguments
-    if n == 0: return lambda: fn(*arguments)
-    elif n == 1: return lambda a0: fn(*arguments)
-    elif n == 2: return lambda a0, a1: fn(*arguments)
-    elif n == 3: return lambda a0, a1, a2: fn(*arguments)
-    elif n == 4: return lambda a0, a1, a2, a3: fn(*arguments)
-    elif n == 5: return lambda a0, a1, a2, a3, a4: fn(*arguments)
-    elif n == 6: return lambda a0, a1, a2, a3, a4, a5: fn(*arguments)
-    elif n == 7: return lambda a0, a1, a2, a3, a4, a5, a6: fn(*arguments)
-    elif n == 8: return lambda a0, a1, a2, a3, a4, a5, a6, a7: fn(*arguments)
-    elif n == 9: return lambda a0, a1, a2, a3, a4, a5, a6, a7, a8: fn(*arguments)
-    elif n == 10: return lambda a0, a1, a2, a3, a4, a5, a6, a7, a8, a9: fn(*arguments)
+def _arity(n, fn):
+    if n == 0: return lambda: fn()
+    elif n == 1: return lambda a0: fn(a0)
+    elif n == 2: return lambda a0, a1: fn(a0, a1)
+    elif n == 3: return lambda a0, a1, a2: fn(a0, a1, a2)
+    elif n == 4: return lambda a0, a1, a2, a3: fn(a0, a1, a2, a3)
+    elif n == 5: return lambda a0, a1, a2, a3, a4: fn(a0, a1, a2, a3, a4)
+    elif n == 6: return lambda a0, a1, a2, a3, a4, a5: fn(a0, a1, a2, a3, a4, a5)
+    elif n == 7: return lambda a0, a1, a2, a3, a4, a5, a6: fn(a0, a1, a2, a3, a4, a5, a6)
+    elif n == 8: return lambda a0, a1, a2, a3, a4, a5, a6, a7: fn(a0, a1, a2, a3, a4, a5, a6, a7)
+    elif n == 9: return lambda a0, a1, a2, a3, a4, a5, a6, a7, a8: fn(a0, a1, a2, a3, a4, a5, a6, a7, a8)
+    elif n == 10: return lambda a0, a1, a2, a3, a4, a5, a6, a7, a8, a9: fn(a0, a1, a2, a3, a4, a5, a6, a7, a8, a9)
     else: raise ValueError('First argument to _arity must be a non-negative integer no greater than ten')
--- source/internal/_isTransformer.py
+++ source/internal/_isTransformer.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
 def _isTransformer(obj):
-    return obj is not None and callable(getattr(obj, '@@transducer/step'))
+    return obj is not None and callable(getattr(obj, '@@transducer/step', None))
