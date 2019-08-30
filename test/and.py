54d0c5982a960141a3e2138715a2e0b5c067c673
Junkang <yuanjunkang@gmail.com>
2019-08-30 16:30:36 +0800

    add test

---
+++ test/and.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+__package__ = 'ramda.test'
+
+from .. import source as R
+from .shared.eq import eq
+
+import unittest
+
+class Test_and(unittest.TestCase):
+    def test_compares_two_values_with_and(self):
+        eq(self, R.and_(True, True), True)
+        eq(self, R.and_(True, False), False)
+        eq(self, R.and_(False, True), False)
+        eq(self, R.and_(False, False), False)
+
+
+if __name__ == '__main__':
+    unittest.main()
