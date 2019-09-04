bcc05185659c68ecb9d90ef21559ee3fb29aa80b
Junkang <yuanjunkang@gmail.com>
2019-09-04 14:02:25 +0800

    delete lens

--- source/over.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from .internal._curry3 import _curry3
-
-Identity = lambda fn, xs: list(map(fn, xs))
-
-@_curry3
-def over(lens, f, x):
-    return lens(Identity)(x)
--- source/view.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from .internal._curry2 import _curry2
-
-Const = lambda fn, xs, *args: xs
-
-@_curry2
-def view(lens, x):
-    return lens(Const)(x)
